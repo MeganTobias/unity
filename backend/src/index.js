ffd899c84b74306f883e468f831eab5ef575d957
ChapmaBeerbohm <paulgrimm691@gmail.com>
2022-06-11 15:30:00 +0800

    feat: create main backend server with Express and middleware setup

---
+++ backend/src/index.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
+
+const logger = require('./utils/logger');
+const errorHandler = require('./middleware/errorHandler');
+const { connectDatabase } = require('./config/database');
+const { connectRedis } = require('./config/redis');
+
+// Import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const assetRoutes = require('./routes/asset');
+const strategyRoutes = require('./routes/strategy');
+const portfolioRoutes = require('./routes/portfolio');
+const riskRoutes = require('./routes/risk');
+const governanceRoutes = require('./routes/governance');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Security middleware
+app.use(helmet());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true
+}));
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.'
+});
+app.use(limiter);
+
+// Compression
+app.use(compression());
+
+// Logging
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
+
+// Body parsing
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+// Health check
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+    version: process.env.npm_package_version || '1.0.0'
+  });
+});
+
+// API routes
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/assets', assetRoutes);
+app.use('/api/strategies', strategyRoutes);
+app.use('/api/portfolio', portfolioRoutes);
+app.use('/api/risk', riskRoutes);
+app.use('/api/governance', governanceRoutes);
+
+// 404 handler
+app.use('*', (req, res) => {
+  res.status(404).json({
+    error: 'Not Found',
+    message: 'The requested resource was not found',
+    path: req.originalUrl
+  });
+});
+
+// Error handling
+app.use(errorHandler);
+
+// Start server
+async function startServer() {
+  try {
+    // Connect to database
+    await connectDatabase();
+    logger.info('Database connected successfully');
+
+    // Connect to Redis
+    await connectRedis();
+    logger.info('Redis connected successfully');
+
+    // Start server
+    app.listen(PORT, () => {
+      logger.info(`Server running on port ${PORT}`);
+      logger.info(`Environment: ${process.env.NODE_ENV || 'development'}`);
+    });
+  } catch (error) {
+    logger.error('Failed to start server:', error);
+    process.exit(1);
+  }
+}
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received, shutting down gracefully');
+  process.exit(0);
+});
+
+process.on('SIGINT', () => {
+  logger.info('SIGINT received, shutting down gracefully');
+  process.exit(0);
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (error) => {
+  logger.error('Uncaught Exception:', error);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
+
+startServer();
+
+module.exports = app;
