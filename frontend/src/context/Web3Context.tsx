f61edfd45a1ac369cfd83eeaedc169629f3c7eca
TrumanGalsworthy <truman.galsworthy@example.com>
2022-10-14 09:40:00 +0800

    feat: add Web3 context provider for global wallet state management

---
+++ frontend/src/context/Web3Context.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, ReactNode } from 'react';
+import { useWeb3 } from '../hooks/useWeb3';
+import { Web3Provider } from '@ethersproject/providers';
+
+interface Web3ContextType {
+  provider: Web3Provider | null;
+  account: string | null;
+  chainId: number | null;
+  isConnecting: boolean;
+  error: string | null;
+  connect: () => Promise<void>;
+  disconnect: () => void;
+  switchNetwork: (chainId: number) => Promise<void>;
+}
+
+const Web3Context = createContext<Web3ContextType | undefined>(undefined);
+
+interface Web3ProviderProps {
+  children: ReactNode;
+}
+
+export const Web3ContextProvider: React.FC<Web3ProviderProps> = ({ children }) => {
+  const web3 = useWeb3();
+
+  return (
+    <Web3Context.Provider value={web3}>
+      {children}
+    </Web3Context.Provider>
+  );
+};
+
+export const useWeb3Context = () => {
+  const context = useContext(Web3Context);
+  if (context === undefined) {
+    throw new Error('useWeb3Context must be used within a Web3ContextProvider');
+  }
+  return context;
+};
+
+export default Web3Context;
