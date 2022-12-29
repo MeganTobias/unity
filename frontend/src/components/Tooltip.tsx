5814f84e481a9082d1d084f864eba04c396ef4c4
TrumanGalsworthy <truman.galsworthy@example.com>
2022-12-29 16:20:00 +0800

    feat: add tooltip component with positioning and delay options

---
+++ frontend/src/components/Tooltip.tsx
@@ -0,0 +1,117 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+interface TooltipProps {
+  children: React.ReactNode;
+  content: string;
+  position?: 'top' | 'bottom' | 'left' | 'right';
+  delay?: number;
+  className?: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({
+  children,
+  content,
+  position = 'top',
+  delay = 200,
+  className = ''
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState({ top: 0, left: 0 });
+  const triggerRef = useRef<HTMLDivElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  const showTooltip = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = setTimeout(() => {
+      setIsVisible(true);
+    }, delay);
+  };
+
+  const hideTooltip = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    setIsVisible(false);
+  };
+
+  useEffect(() => {
+    if (isVisible && triggerRef.current && tooltipRef.current) {
+      const triggerRect = triggerRef.current.getBoundingClientRect();
+      const tooltipRect = tooltipRef.current.getBoundingClientRect();
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+      let top = 0;
+      let left = 0;
+
+      switch (position) {
+        case 'top':
+          top = triggerRect.top + scrollTop - tooltipRect.height - 8;
+          left = triggerRect.left + scrollLeft + (triggerRect.width - tooltipRect.width) / 2;
+          break;
+        case 'bottom':
+          top = triggerRect.bottom + scrollTop + 8;
+          left = triggerRect.left + scrollLeft + (triggerRect.width - tooltipRect.width) / 2;
+          break;
+        case 'left':
+          top = triggerRect.top + scrollTop + (triggerRect.height - tooltipRect.height) / 2;
+          left = triggerRect.left + scrollLeft - tooltipRect.width - 8;
+          break;
+        case 'right':
+          top = triggerRect.top + scrollTop + (triggerRect.height - tooltipRect.height) / 2;
+          left = triggerRect.right + scrollLeft + 8;
+          break;
+      }
+
+      setTooltipPosition({ top, left });
+    }
+  }, [isVisible, position]);
+
+  const positionClasses = {
+    top: 'bottom-full left-1/2 transform -translate-x-1/2 mb-2',
+    bottom: 'top-full left-1/2 transform -translate-x-1/2 mt-2',
+    left: 'right-full top-1/2 transform -translate-y-1/2 mr-2',
+    right: 'left-full top-1/2 transform -translate-y-1/2 ml-2'
+  };
+
+  const arrowClasses = {
+    top: 'top-full left-1/2 transform -translate-x-1/2 border-t-gray-900',
+    bottom: 'bottom-full left-1/2 transform -translate-x-1/2 border-b-gray-900',
+    left: 'left-full top-1/2 transform -translate-y-1/2 border-l-gray-900',
+    right: 'right-full top-1/2 transform -translate-y-1/2 border-r-gray-900'
+  };
+
+  return (
+    <div
+      ref={triggerRef}
+      className={`relative inline-block ${className}`}
+      onMouseEnter={showTooltip}
+      onMouseLeave={hideTooltip}
+      onFocus={showTooltip}
+      onBlur={hideTooltip}
+    >
+      {children}
+      
+      {isVisible && (
+        <div
+          ref={tooltipRef}
+          className={`absolute z-50 px-2 py-1 text-sm text-white bg-gray-900 rounded shadow-lg whitespace-nowrap ${positionClasses[position]}`}
+          style={{
+            top: tooltipPosition.top,
+            left: tooltipPosition.left
+          }}
+        >
+          {content}
+          <div
+            className={`absolute w-0 h-0 border-4 border-transparent ${arrowClasses[position]}`}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
