ad967b653b23a83edebfbc50d2004bc0c456290f
MeganTobias <megan.tobias@example.com>
2023-01-03 09:40:00 +0800

    feat: add progress bar component with multiple variants and size options

---
+++ frontend/src/components/ProgressBar.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+interface ProgressBarProps {
+  value: number;
+  max?: number;
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'default' | 'success' | 'warning' | 'error';
+  showLabel?: boolean;
+  label?: string;
+  className?: string;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({
+  value,
+  max = 100,
+  size = 'md',
+  variant = 'default',
+  showLabel = false,
+  label,
+  className = ''
+}) => {
+  const percentage = Math.min(Math.max((value / max) * 100, 0), 100);
+
+  const sizeClasses = {
+    sm: 'h-2',
+    md: 'h-3',
+    lg: 'h-4'
+  };
+
+  const variantClasses = {
+    default: 'bg-blue-600',
+    success: 'bg-green-600',
+    warning: 'bg-yellow-600',
+    error: 'bg-red-600'
+  };
+
+  const labelSizeClasses = {
+    sm: 'text-xs',
+    md: 'text-sm',
+    lg: 'text-base'
+  };
+
+  return (
+    <div className={`w-full ${className}`}>
+      {showLabel && (
+        <div className="flex justify-between items-center mb-1">
+          <span className={`font-medium text-gray-700 ${labelSizeClasses[size]}`}>
+            {label || `${Math.round(percentage)}%`}
+          </span>
+          <span className={`text-gray-500 ${labelSizeClasses[size]}`}>
+            {value}/{max}
+          </span>
+        </div>
+      )}
+      
+      <div className={`w-full bg-gray-200 rounded-full overflow-hidden ${sizeClasses[size]}`}>
+        <div
+          className={`h-full transition-all duration-300 ease-in-out ${variantClasses[variant]}`}
+          style={{ width: `${percentage}%` }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
