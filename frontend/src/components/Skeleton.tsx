4babaaf856b0c5570fafc726db31b18039b2e6d1
TrumanGalsworthy <truman.galsworthy@example.com>
2023-01-27 16:00:00 +0800

    feat: add skeleton component for loading states with multiple variants

---
+++ frontend/src/components/Skeleton.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+interface SkeletonProps {
+  width?: string | number;
+  height?: string | number;
+  variant?: 'text' | 'rectangular' | 'circular';
+  animation?: 'pulse' | 'wave' | 'none';
+  className?: string;
+}
+
+const Skeleton: React.FC<SkeletonProps> = ({
+  width = '100%',
+  height = '1rem',
+  variant = 'rectangular',
+  animation = 'pulse',
+  className = ''
+}) => {
+  const widthStyle = typeof width === 'number' ? `${width}px` : width;
+  const heightStyle = typeof height === 'number' ? `${height}px` : height;
+
+  const variantClasses = {
+    text: 'h-4',
+    rectangular: '',
+    circular: 'rounded-full'
+  };
+
+  const animationClasses = {
+    pulse: 'animate-pulse',
+    wave: 'animate-pulse',
+    none: ''
+  };
+
+  const baseClasses = 'bg-gray-200 rounded';
+
+  return (
+    <div
+      className={`
+        ${baseClasses}
+        ${variantClasses[variant]}
+        ${animationClasses[animation]}
+        ${className}
+      `}
+      style={{
+        width: widthStyle,
+        height: heightStyle
+      }}
+    />
+  );
+};
+
+export default Skeleton;
