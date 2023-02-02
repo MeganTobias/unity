296286230105596290bb6e2a5d8f14f53cc48c47
AmosCroft <amos.croft@example.com>
2023-02-02 10:40:00 +0800

    feat: add toast container component with global toast management

---
+++ frontend/src/components/ToastContainer.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useCallback } from 'react';
+import Toast, { ToastProps } from './Toast';
+
+interface ToastContainerProps {
+  children: React.ReactNode;
+}
+
+interface ToastData extends Omit<ToastProps, 'onClose'> {}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ children }) => {
+  const [toasts, setToasts] = useState<ToastData[]>([]);
+
+  const addToast = useCallback((toast: Omit<ToastData, 'id'>) => {
+    const id = Math.random().toString(36).substr(2, 9);
+    const newToast = { ...toast, id };
+    setToasts(prev => [...prev, newToast]);
+  }, []);
+
+  const removeToast = useCallback((id: string) => {
+    setToasts(prev => prev.filter(toast => toast.id !== id));
+  }, []);
+
+  const showSuccess = useCallback((title: string, message?: string, duration?: number) => {
+    addToast({ type: 'success', title, message, duration });
+  }, [addToast]);
+
+  const showError = useCallback((title: string, message?: string, duration?: number) => {
+    addToast({ type: 'error', title, message, duration });
+  }, [addToast]);
+
+  const showWarning = useCallback((title: string, message?: string, duration?: number) => {
+    addToast({ type: 'warning', title, message, duration });
+  }, [addToast]);
+
+  const showInfo = useCallback((title: string, message?: string, duration?: number) => {
+    addToast({ type: 'info', title, message, duration });
+  }, [addToast]);
+
+  // Expose toast methods to children via context or ref
+  React.useEffect(() => {
+    (window as any).toast = {
+      success: showSuccess,
+      error: showError,
+      warning: showWarning,
+      info: showInfo
+    };
+  }, [showSuccess, showError, showWarning, showInfo]);
+
+  return (
+    <>
+      {children}
+      <div className="fixed top-4 right-4 z-50 space-y-2">
+        {toasts.map(toast => (
+          <Toast
+            key={toast.id}
+            {...toast}
+            onClose={removeToast}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ToastContainer;
