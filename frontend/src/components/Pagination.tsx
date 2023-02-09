c6b1f91536c450104c2f4784c1714016bc3837ef
TrumanGalsworthy <truman.galsworthy@example.com>
2023-02-09 12:20:00 +0800

    feat: add pagination component with ellipsis and customizable options

---
+++ frontend/src/components/Pagination.tsx
@@ -0,0 +1,179 @@
+import React from 'react';
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+  showFirstLast?: boolean;
+  showPrevNext?: boolean;
+  maxVisiblePages?: number;
+  size?: 'sm' | 'md' | 'lg';
+  className?: string;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+  showFirstLast = true,
+  showPrevNext = true,
+  maxVisiblePages = 5,
+  size = 'md',
+  className = ''
+}) => {
+  const sizeClasses = {
+    sm: 'px-2 py-1 text-xs',
+    md: 'px-3 py-2 text-sm',
+    lg: 'px-4 py-2 text-base'
+  };
+
+  const getVisiblePages = () => {
+    const pages: (number | string)[] = [];
+    const half = Math.floor(maxVisiblePages / 2);
+    
+    let start = Math.max(1, currentPage - half);
+    let end = Math.min(totalPages, currentPage + half);
+    
+    // Adjust if we're near the beginning or end
+    if (currentPage <= half) {
+      end = Math.min(totalPages, maxVisiblePages);
+    }
+    if (currentPage > totalPages - half) {
+      start = Math.max(1, totalPages - maxVisiblePages + 1);
+    }
+    
+    // Add first page and ellipsis if needed
+    if (start > 1) {
+      pages.push(1);
+      if (start > 2) {
+        pages.push('...');
+      }
+    }
+    
+    // Add visible pages
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+    
+    // Add ellipsis and last page if needed
+    if (end < totalPages) {
+      if (end < totalPages - 1) {
+        pages.push('...');
+      }
+      pages.push(totalPages);
+    }
+    
+    return pages;
+  };
+
+  const handlePageChange = (page: number) => {
+    if (page >= 1 && page <= totalPages && page !== currentPage) {
+      onPageChange(page);
+    }
+  };
+
+  if (totalPages <= 1) return null;
+
+  return (
+    <nav className={`flex items-center justify-center space-x-1 ${className}`}>
+      {showFirstLast && (
+        <button
+          onClick={() => handlePageChange(1)}
+          disabled={currentPage === 1}
+          className={`
+            ${sizeClasses[size]}
+            font-medium rounded-md
+            ${currentPage === 1
+              ? 'text-gray-400 cursor-not-allowed'
+              : 'text-gray-700 hover:bg-gray-50 hover:text-gray-900'
+            }
+            transition-colors duration-200
+          `}
+        >
+          First
+        </button>
+      )}
+      
+      {showPrevNext && (
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className={`
+            ${sizeClasses[size]}
+            font-medium rounded-md
+            ${currentPage === 1
+              ? 'text-gray-400 cursor-not-allowed'
+              : 'text-gray-700 hover:bg-gray-50 hover:text-gray-900'
+            }
+            transition-colors duration-200
+          `}
+        >
+          Previous
+        </button>
+      )}
+      
+      {getVisiblePages().map((page, index) => (
+        <React.Fragment key={index}>
+          {page === '...' ? (
+            <span className={`${sizeClasses[size]} text-gray-500`}>
+              ...
+            </span>
+          ) : (
+            <button
+              onClick={() => handlePageChange(page as number)}
+              className={`
+                ${sizeClasses[size]}
+                font-medium rounded-md
+                ${currentPage === page
+                  ? 'bg-blue-600 text-white'
+                  : 'text-gray-700 hover:bg-gray-50 hover:text-gray-900'
+                }
+                transition-colors duration-200
+              `}
+            >
+              {page}
+            </button>
+          )}
+        </React.Fragment>
+      ))}
+      
+      {showPrevNext && (
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className={`
+            ${sizeClasses[size]}
+            font-medium rounded-md
+            ${currentPage === totalPages
+              ? 'text-gray-400 cursor-not-allowed'
+              : 'text-gray-700 hover:bg-gray-50 hover:text-gray-900'
+            }
+            transition-colors duration-200
+          `}
+        >
+          Next
+        </button>
+      )}
+      
+      {showFirstLast && (
+        <button
+          onClick={() => handlePageChange(totalPages)}
+          disabled={currentPage === totalPages}
+          className={`
+            ${sizeClasses[size]}
+            font-medium rounded-md
+            ${currentPage === totalPages
+              ? 'text-gray-400 cursor-not-allowed'
+              : 'text-gray-700 hover:bg-gray-50 hover:text-gray-900'
+            }
+            transition-colors duration-200
+          `}
+        >
+          Last
+        </button>
+      )}
+    </nav>
+  );
+};
+
+export default Pagination;
