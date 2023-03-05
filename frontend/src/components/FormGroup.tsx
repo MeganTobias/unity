af977130069dc3c924ec09e39ccd1f6ad9bced2c
TrumanGalsworthy <truman.galsworthy@example.com>
2023-03-05 12:00:00 +0800

    feat: add form group component for form layout and validation display

---
+++ frontend/src/components/FormGroup.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+interface FormGroupProps {
+  children: React.ReactNode;
+  label?: string;
+  error?: string;
+  helperText?: string;
+  required?: boolean;
+  className?: string;
+}
+
+const FormGroup: React.FC<FormGroupProps> = ({
+  children,
+  label,
+  error,
+  helperText,
+  required = false,
+  className = ''
+}) => {
+  return (
+    <div className={`space-y-1 ${className}`}>
+      {label && (
+        <label className="block text-sm font-medium text-gray-700">
+          {label}
+          {required && <span className="text-red-500 ml-1">*</span>}
+        </label>
+      )}
+      
+      <div className="space-y-2">
+        {children}
+      </div>
+      
+      {error && (
+        <p className="text-sm text-red-600">{error}</p>
+      )}
+      
+      {helperText && !error && (
+        <p className="text-sm text-gray-500">{helperText}</p>
+      )}
+    </div>
+  );
+};
+
+export default FormGroup;
