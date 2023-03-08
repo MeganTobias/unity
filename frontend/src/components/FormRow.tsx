b6da0df8f5117b02752aabfabf5aa23b3c70da46
MeganTobias <megan.tobias@example.com>
2023-03-08 13:20:00 +0800

    feat: add form row component for responsive form layout

---
+++ frontend/src/components/FormRow.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+interface FormRowProps {
+  children: React.ReactNode;
+  columns?: 1 | 2 | 3 | 4 | 6 | 12;
+  gap?: 'sm' | 'md' | 'lg';
+  className?: string;
+}
+
+const FormRow: React.FC<FormRowProps> = ({
+  children,
+  columns = 1,
+  gap = 'md',
+  className = ''
+}) => {
+  const gapClasses = {
+    sm: 'gap-2',
+    md: 'gap-4',
+    lg: 'gap-6'
+  };
+
+  const columnClasses = {
+    1: 'grid-cols-1',
+    2: 'grid-cols-1 md:grid-cols-2',
+    3: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3',
+    4: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-4',
+    6: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6',
+    12: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6'
+  };
+
+  return (
+    <div className={`grid ${columnClasses[columns]} ${gapClasses[gap]} ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default FormRow;
