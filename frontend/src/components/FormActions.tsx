b2fa07fd93108fe66e7a94e07210d2e07c3e2fb7
AmosCroft <amos.croft@example.com>
2023-03-11 14:40:00 +0800

    feat: add form actions component for form button layout and actions

---
+++ frontend/src/components/FormActions.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Button from './Button';
+
+interface FormActionsProps {
+  onSubmit?: () => void;
+  onReset?: () => void;
+  onCancel?: () => void;
+  submitText?: string;
+  resetText?: string;
+  cancelText?: string;
+  loading?: boolean;
+  disabled?: boolean;
+  showReset?: boolean;
+  showCancel?: boolean;
+  align?: 'left' | 'center' | 'right';
+  className?: string;
+}
+
+const FormActions: React.FC<FormActionsProps> = ({
+  onSubmit,
+  onReset,
+  onCancel,
+  submitText = 'Submit',
+  resetText = 'Reset',
+  cancelText = 'Cancel',
+  loading = false,
+  disabled = false,
+  showReset = true,
+  showCancel = false,
+  align = 'right',
+  className = ''
+}) => {
+  const alignClasses = {
+    left: 'justify-start',
+    center: 'justify-center',
+    right: 'justify-end'
+  };
+
+  return (
+    <div className={`flex items-center space-x-3 ${alignClasses[align]} ${className}`}>
+      {showCancel && (
+        <Button
+          type="button"
+          variant="outline"
+          onClick={onCancel}
+          disabled={disabled || loading}
+        >
+          {cancelText}
+        </Button>
+      )}
+      
+      {showReset && (
+        <Button
+          type="button"
+          variant="outline"
+          onClick={onReset}
+          disabled={disabled || loading}
+        >
+          {resetText}
+        </Button>
+      )}
+      
+      <Button
+        type="submit"
+        variant="primary"
+        onClick={onSubmit}
+        loading={loading}
+        disabled={disabled}
+      >
+        {submitText}
+      </Button>
+    </div>
+  );
+};
+
+export default FormActions;
