8f9c1452e3356be1fa10d14e3212c13605326a2c
MeganTobias <megan.tobias@example.com>
2023-03-21 09:40:00 +0800

    feat: add form helper text component with multiple variants

---
+++ frontend/src/components/FormHelperText.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface FormHelperTextProps {
+  children: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'default' | 'success' | 'warning' | 'error';
+  className?: string;
+}
+
+const FormHelperText: React.FC<FormHelperTextProps> = ({
+  children,
+  size = 'md',
+  variant = 'default',
+  className = ''
+}) => {
+  const sizeClasses = {
+    sm: 'text-xs',
+    md: 'text-sm',
+    lg: 'text-base'
+  };
+
+  const variantClasses = {
+    default: 'text-gray-500',
+    success: 'text-green-600',
+    warning: 'text-yellow-600',
+    error: 'text-red-600'
+  };
+
+  return (
+    <p className={`${sizeClasses[size]} ${variantClasses[variant]} ${className}`}>
+      {children}
+    </p>
+  );
+};
+
+export default FormHelperText;
