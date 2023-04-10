42432a5ec9e3f10eb23804bc0571d80977ff68e5
ChapmaBeerbohm <chapma.beerbohm@example.com>
2023-04-10 15:40:00 +0800

    feat: add form legend component for fieldset legends

---
+++ frontend/src/components/FormLegend.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface FormLegendProps {
+  children: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';
+  required?: boolean;
+  className?: string;
+}
+
+const FormLegend: React.FC<FormLegendProps> = ({
+  children,
+  size = 'md',
+  required = false,
+  className = ''
+}) => {
+  const sizeClasses = {
+    sm: 'text-sm',
+    md: 'text-base',
+    lg: 'text-lg'
+  };
+
+  return (
+    <legend className={`font-medium text-gray-900 ${sizeClasses[size]} ${className}`}>
+      {children}
+      {required && <span className="text-red-500 ml-1">*</span>}
+    </legend>
+  );
+};
+
+export default FormLegend;
