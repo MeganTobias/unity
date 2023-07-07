5f8cabcf7c80f30ff664f72be1906884bd69f776
ChapmaBeerbohm <chapma.beerbohm@example.com>
2023-07-07 13:45:00 +0800

    feat: add form field description component for field descriptions

---
+++ frontend/src/components/FormFieldDescription.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+interface FormFieldDescriptionProps {
+  children: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'default' | 'muted' | 'success' | 'warning' | 'error' | 'info';
+  className?: string;
+}
+
+const FormFieldDescription: React.FC<FormFieldDescriptionProps> = ({
+  children,
+  size = 'md',
+  variant = 'default',
+  className = ''
+}) => {
+  const sizeClasses = {
+    sm: 'text-sm',
+    md: 'text-base',
+    lg: 'text-lg'
+  };
+
+  const variantClasses = {
+    default: 'text-gray-700',
+    muted: 'text-gray-500',
+    success: 'text-green-600',
+    warning: 'text-yellow-600',
+    error: 'text-red-600',
+    info: 'text-blue-600'
+  };
+
+  return (
+    <p className={`${sizeClasses[size]} ${variantClasses[variant]} ${className}`}>
+      {children}
+    </p>
+  );
+};
+
+export default FormFieldDescription;
