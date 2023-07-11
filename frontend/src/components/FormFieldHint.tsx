1aaffc5eeb603f9edbdfe59f588df85ebe020205
TrumanGalsworthy <truman.galsworthy@example.com>
2023-07-11 14:10:00 +0800

    feat: add form field hint component for field hints

---
+++ frontend/src/components/FormFieldHint.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface FormFieldHintProps {
+  children: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'default' | 'success' | 'warning' | 'error' | 'info';
+  className?: string;
+}
+
+const FormFieldHint: React.FC<FormFieldHintProps> = ({
+  children,
+  size = 'md',
+  variant = 'default',
+  className = ''
+}) => {
+  const sizeClasses = {
+    sm: 'text-sm',
+    md: 'text-base',
+    lg: 'text-lg'
+  };
+
+  const variantClasses = {
+    default: 'text-gray-500',
+    success: 'text-green-600',
+    warning: 'text-yellow-600',
+    error: 'text-red-600',
+    info: 'text-blue-600'
+  };
+
+  return (
+    <p className={`${sizeClasses[size]} ${variantClasses[variant]} ${className}`}>
+      {children}
+    </p>
+  );
+};
+
+export default FormFieldHint;
