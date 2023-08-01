eca671ae2b427ff6cadec8d69027a0854511c9cd
AmosCroft <amos.croft@example.com>
2023-08-01 12:40:00 +0800

    feat: add form field error component for field error messages with icons

--- frontend/src/components/FormFieldError.tsx
+++ frontend/src/components/FormFieldError.tsx
@@ -26,9 +26,22 @@ const FormFieldError: React.FC<FormFieldErrorProps> = ({
   };
 
   return (
-    <p className={`${sizeClasses[size]} ${variantClasses[variant]} ${className}`}>
-      {children}
-    </p>
+    <div className={`flex items-start space-x-2 ${className}`}>
+      <svg
+        className="w-4 h-4 text-red-500 mt-0.5 flex-shrink-0"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+          clipRule="evenodd"
+        />
+      </svg>
+      <p className={`${sizeClasses[size]} ${variantClasses[variant]}`}>
+        {children}
+      </p>
+    </div>
   );
 };
 
