89a1dd40a9a4eda84fb4efce4a90b6b3f776638b
AmosCroft <amos.croft@example.com>
2023-10-24 13:05:00 +0800

    feat: add form field grid component for field grid layout

---
+++ frontend/src/components/FormFieldGrid.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+interface FormFieldGridProps {
+  children: React.ReactNode;
+  cols?: 1 | 2 | 3 | 4 | 5 | 6 | 12;
+  gap?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  className?: string;
+}
+
+const FormFieldGrid: React.FC<FormFieldGridProps> = ({
+  children,
+  cols = 2,
+  gap = 'md',
+  className = ''
+}) => {
+  const colsClasses = {
+    1: 'grid-cols-1',
+    2: 'grid-cols-2',
+    3: 'grid-cols-3',
+    4: 'grid-cols-4',
+    5: 'grid-cols-5',
+    6: 'grid-cols-6',
+    12: 'grid-cols-12'
+  };
+
+  const gapClasses = {
+    xs: 'gap-1',
+    sm: 'gap-2',
+    md: 'gap-4',
+    lg: 'gap-6',
+    xl: 'gap-8'
+  };
+
+  return (
+    <div className={`grid ${colsClasses[cols]} ${gapClasses[gap]} ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default FormFieldGrid;
