d1e08664dcc5254cd9808f3400d1411a12effd5c
TrumanGalsworthy <truman.galsworthy@example.com>
2023-11-27 15:15:00 +0800

    feat: add form field hint component for field hints

--- frontend/src/components/FormFieldHint.tsx
+++ frontend/src/components/FormFieldHint.tsx
@@ -28,11 +28,23 @@ const FormFieldHint: React.FC<FormFieldHintProps> = ({
   };
 
   return (
-    <p className={`${sizeClasses[size]} ${variantClasses[variant]} ${className}`}>
-      {children}
-    </p>
+    <div className={`flex items-start space-x-2 ${className}`}>
+      <svg
+        className="w-4 h-4 text-gray-400 mt-0.5 flex-shrink-0"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          fillRule="evenodd"
+          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+          clipRule="evenodd"
+        />
+      </svg>
+      <p className={`${sizeClasses[size]} ${variantClasses[variant]}`}>
+        {children}
+      </p>
+    </div>
   );
 };
 
 export default FormFieldHint;
-
